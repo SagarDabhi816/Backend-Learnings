01ca8e814634876afbff07decaac635582eb12ba
Sagardabhi816 <sagardabhi816@gmail.com>
2026-01-23 19:16:00 +0530

    Changes in repo

--- Day-7/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-function connectToDB(){
-        mongoose.connect("mongodb+srv://sagardabhi816_db_user:12345666@cluster0.kjrqfcr.mongodb.net/cohort").then(()=>{
-            console.log("Yeahhhh boiiii")
-        })
-}
-
-module.exports = connectToDB
