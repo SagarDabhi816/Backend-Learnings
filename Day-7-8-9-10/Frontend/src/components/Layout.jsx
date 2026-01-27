4c906e89e070cb9fd4d09b624be62494f1db849a
Sagardabhi816 <sagardabhi816@gmail.com>
2026-01-27 23:01:48 +0530

    Moody Player Project Complete

--- Day-7-8-9-10/Frontend/src/components/Layout.jsx
+++ Day-7-8-9-10/Frontend/src/components/Layout.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 const Layout = ({ children }) => {
   return (
-    <main className="px-[15%]  text-[#1b1b1b] w-2/3 select-none flex flex-col items-center justify-center">
+    <main className="px-[15%] w-3/4 select-none flex flex-col items-center justify-center">
       {children}
     </main>
   );
