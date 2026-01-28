4027bc0ee9e635b37033dd01d3e669a40d4d28d8
Sagardabhi816 <sagardabhi816@gmail.com>
2026-01-28 19:27:14 +0530

    Initial Setup

---
+++ Day-11/src/routes/index.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+
+router.get("",(req,res)=>{
+
+    res.json({
+        message:"Get route working"
+    })
+})
+
+module.exports = router;
